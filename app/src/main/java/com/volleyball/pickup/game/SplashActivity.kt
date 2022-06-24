01596c404c44322a5e9cb59ee3ffb671dddaef6a
Henry Ye <jianheng.ye@icloud.com>
2022-06-24 17:05:06 +0800

    Add splash screen.

---
+++ app/src/main/java/com/volleyball/pickup/game/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.volleyball.pickup.game
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
